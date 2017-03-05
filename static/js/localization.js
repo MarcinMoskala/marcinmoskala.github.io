f6963abd43b30df5b1b5019bcddbcdef8a23b3d1
MarcinMoskala <marcinmoskala@gmail.com>
2017-03-05 10:22:03 +0100

    Connect lang to url without refreshing

--- static/js/localization.js
+++ static/js/localization.js
@@ -1,3 +1,24 @@
+function getQueryParam(param) {
+    location.search.substr(1)
+        .split("&")
+        .some(function(item) { // returns first occurence and stops
+            return item.split("=")[0] == param && (param = item.split("=")[1])
+        })
+    return param
+}
+
+function replaceQueryParam(param, newval) {
+    var regex = new RegExp("([?;&])" + param + "[^&;]*[;&]?");
+    var query = window.location.search.replace(regex, "$1").replace(/&$/, '');
+    window.location = window.location.pathname + (query.length > 2 ? query + "&" : "?") + (newval ? param + "=" + newval : '');
+}
+
+function quietReplaceQueryParam(param, newval) {
+    var regex = new RegExp("([?;&])" + param + "[^&;]*[;&]?");
+    var query = window.location.search.replace(regex, "$1").replace(/&$/, '');
+    history.pushState({}, null, window.location.pathname + (query.length > 2 ? query + "&" : "?") + (newval ? param + "=" + newval : ''));
+}
+
 $(document).ready(function() {
   var i18nOpts = {
     lng: 'en',
@@ -20,9 +41,16 @@ $(document).ready(function() {
   $("[id^=set_lang]").each(function() {
     var $this = $(this);
     $this.on('click', function() {
-      i18n.setLng($this.data('locale'), translate);
+      var lang = $this.data('locale')
+      i18n.setLng(lang, translate);
+      quietReplaceQueryParam('l', lang);
     });
   });
 
   $.i18n.init(i18nOpts, translate);
+
+  var queryLang = getQueryParam('l');
+  if(queryLang !== null) {
+    i18n.setLng(queryLang, translate);
+  }
 });
