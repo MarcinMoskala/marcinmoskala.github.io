0ef79bb891c51f45b90d97c00e09a85776eff018
Marcin Moskala <marcinmoskala@gmail.com>
2017-05-05 09:24:22 +0200

    Add video

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -26,7 +26,7 @@ module Jekyll
     end
 
     def render(context)
-      source = "<div class=\"video\">"
+      source = "<div class=\"video\" style="width: 50%; margin: 0 auto;">"
       source += "<figure>"
       source += "<iframe width=\"640\" height=\"480\" src=\"//www.youtube.com/embed/#{@url}\" frameborder=\"0\" allowfullscreen></iframe>"
       source += "</figure>"
