85c9593d912610a61b9afcc2ea7d9ec4a7a3f38b
Marcin Moskala <marcinmoskala@gmail.com>
2018-06-17 10:47:13 +0200

    Update and priorities change

--- static/js/localization.js
+++ static/js/localization.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
     $('.i18container').i18n();
     $('#i18_navbar').i18n();
     $('#i18_about_me').i18n();
-    $('#i18_skills').i18n();
+    $('#i18_services').i18n();
     $('#i18_projects').i18n();
     $('#i18_career').i18n();
     $('#i18_talks').i18n();
